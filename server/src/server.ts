35162f649c7c1ba27b7ccff2ecb3b66397ebeb59
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-09-14 18:03:45 -0300

    ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId =request.params.id;
+    
     return response.json([
         { id: 1, nome: 'game' },
         { id: 2, nome: 'game' },
